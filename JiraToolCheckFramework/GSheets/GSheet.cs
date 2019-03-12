884c5e63779ed539c7998e897fab3d12416a9da6
vtrencasnky <vtrencansky@i4industry.eu>
2019-03-12 15:29:29 +0100

    Conditional format in GSheet class

--- JiraToolCheckFramework/GSheets/GSheet.cs
+++ JiraToolCheckFramework/GSheets/GSheet.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Services;
 using Google.Apis.Sheets.v4;
@@ -19,6 +20,10 @@ namespace JiraToolCheckFramework.GSheets
          private const string SHEET_START_RANGE = "A:A";
          private const string GOOGLE_API_CREDENTIALS_PATH = "credentials.json";
          private const string GOOGLE_API_TOKEN_NAME = "token.json";
+         private const string InterpolationTypeNumber = "Number";
+         private readonly Color _redColor = new Color { Red = 1 };
+         private readonly Color _greenColor = new Color { Green = 1 };
+
          private static SheetsService _service;
          private static SheetsService Service => _service ?? (_service = Initialize());
 
@@ -70,14 +75,56 @@ namespace JiraToolCheckFramework.GSheets
             request.InsertDataOption = SpreadsheetsResource.ValuesResource.AppendRequest.InsertDataOptionEnum.OVERWRITE;
             request.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
             var response = request.Execute();
+            SetConditionalFormatting();
          }
 
          public void ClearSheet(string sheetName)
          {
             ClearValuesRequest requestBody = new ClearValuesRequest();
-            
+
             SpreadsheetsResource.ValuesResource.ClearRequest clearRequest = Service.Spreadsheets.Values.Clear(requestBody, SheetId, GetSheetAndRangeName(sheetName, ALL_COLUMNS_RANGE));
             clearRequest.Execute();
+         }
+
+         private void SetConditionalFormatting()
+         {
+            Spreadsheet spr = Service.Spreadsheets.Get(SheetId).Execute();
+            Sheet sh = spr.Sheets.FirstOrDefault(s => s.Properties.Title == "attendance");
+            int sheetId = (int)sh.Properties.SheetId;
+
+            var conditionalFormatRule = new ConditionalFormatRule
+            {
+               GradientRule = new GradientRule
+               {
+                  Maxpoint = new InterpolationPoint { Color = _redColor, Type = InterpolationTypeNumber, Value = "12" },
+                  Midpoint = new InterpolationPoint { Color = _greenColor, Type = InterpolationTypeNumber, Value = "8" },
+                  Minpoint = new InterpolationPoint { Color = _redColor, Type = InterpolationTypeNumber, Value = "6" }
+               },
+               Ranges = new List<GridRange>
+               {
+                  new GridRange
+                  {
+                     SheetId = sheetId,
+                     StartColumnIndex = 1,
+                     StartRowIndex = 2
+                  }
+               }
+            };
+
+            var batchUpdateSpreadsheetRequest = new BatchUpdateSpreadsheetRequest();
+
+            var updateCellsRequest = new Request
+            {
+               AddConditionalFormatRule = new AddConditionalFormatRuleRequest
+               {
+                  Rule = conditionalFormatRule
+               },
+               
+            };
+
+            batchUpdateSpreadsheetRequest.Requests = new List<Request> { updateCellsRequest};
+            var bur = Service.Spreadsheets.BatchUpdate(batchUpdateSpreadsheetRequest, SheetId);
+            bur.Execute();
          }
 
          public void DeleteAllRowsAndColumns(string sheetName)
@@ -87,12 +134,12 @@ namespace JiraToolCheckFramework.GSheets
 
             DeleteDimensionRequest deleteRowsRequest = new DeleteDimensionRequest
             {
-               Range = new DimensionRange {Dimension = "ROWS", StartIndex = 0, EndIndex = 500000}
+               Range = new DimensionRange { Dimension = "ROWS", StartIndex = 0, EndIndex = 500000 }
             };
 
             DeleteDimensionRequest deleteColumnsRequest = new DeleteDimensionRequest
             {
-               Range = new DimensionRange {Dimension = "COLUMNS", StartIndex = 0, EndIndex = 500000 }
+               Range = new DimensionRange { Dimension = "COLUMNS", StartIndex = 0, EndIndex = 500000 }
             };
             requestBody.Requests = new List<Request>
             {
