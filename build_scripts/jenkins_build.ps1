b4289b47a62be241d5f301a684661390ef1582ce
Michal Štefanec <mrstefi92@gmail.com>
2019-05-29 14:58:58 +0200

    change file to match also HRReports

--- build_scripts/jenkins_build.ps1
+++ build_scripts/jenkins_build.ps1
@@ -1,3 +1,4 @@
 powershell -Command "(gc .\PRJReports\App.config) -replace '<Database connection string>', 'bar' | Out-File -encoding ASCII .\PRJReports\App.config"
 nuget restore .\JiraReporters.sln 
 msbuild .\PRJReports\PRJReports.csproj
+msbuild .\HRReports\HRReports.csproj
